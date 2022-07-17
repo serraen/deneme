ca6163e129fea4732b4ae57599c69f5aa90a5470
serraen <lavanta1134@gmail.com>
2022-07-17 14:55:37 +0200

    third deneme

--- src/Serra.java
+++ src/Serra.java
@@ -2,5 +2,6 @@ public class Serra {
     public static void main(String[] args) {
         System.out.println("Deneme");
         System.out.println("Deneme iki");
+        System.out.println("Deneme uc");
     }
 }
